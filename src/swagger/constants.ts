aa5eb75f9a88988167d7c35713647e920137d4b4
sachin-ezee <sachin.devaiya@ezeetechnosys.com>
2021-09-13 12:35:18 +0530

    code add

--- src/swagger/constants.ts
+++ src/swagger/constants.ts
@@ -1,4 +1,4 @@
 export const SWAGGER_API_ROOT = 'api/docs';
-export const SWAGGER_API_NAME = 'Simple API';
-export const SWAGGER_API_DESCRIPTION = 'Simple API Description';
+export const SWAGGER_API_NAME = 'Nest Js API';
+export const SWAGGER_API_DESCRIPTION = 'Nest Js API Description';
 export const SWAGGER_API_CURRENT_VERSION = '1.0';
