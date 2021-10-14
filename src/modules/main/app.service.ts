20fdd195dc9f38bce369a2e1a3128b6d699377c1
sachin-ezee <sachin.devaiya@ezeetechnosys.com>
2021-10-14 11:41:09 +0530

    cron code comment

--- src/modules/main/app.service.ts
+++ src/modules/main/app.service.ts
@@ -8,20 +8,25 @@ export class AppService {
 
   private readonly logger = new Logger(AppService.name);
 
-  @Cron('45 * * * * *')
-  handleCron() {
-    this.logger.debug('Called when the current second is 20');
-  }
+  // @Cron('20 * * * * *')
+  // handleCron() {
+  //   this.logger.debug('Called when the second is 20');
+  // }
 
-  @Interval(10000)
-  handleInterval() {
-    this.logger.debug('Called every 10 seconds');
-  }
+  // @Cron('45 * * * * *')
+  // handleCron2() {
+  //   this.logger.debug('Called when the current second is 45');
+  // }
 
-  @Timeout(5000)
-  handleTimeout() {
-    this.logger.debug('Called once after 5 seconds');
-  }
+  // @Interval(10000)
+  // handleInterval() {
+  //   this.logger.debug('Called every 10 seconds');
+  // }
+
+  // @Timeout(5000)
+  // handleTimeout() {
+  //   this.logger.debug('Called once after 5 seconds');
+  // }
 
   root(): string {
     return this.config.get('APP_URL');
